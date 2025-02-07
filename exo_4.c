d08c68174bdc8fd3e6bf110a76011ca7b96902a4
TsikyAndriatahiana <antsah.andriatahina@gmail.com>
2025-02-07 09:21:38 +0300

    Update exo_4.c

--- exo_4.c
+++ exo_4.c
@@ -6,7 +6,7 @@ int main ()
     scanf ("%d",&a);
     scanf ("%d",&b);
     s=a+b;
-    printf ("la somme des deux notes est %d\n",s),
+    printf ("la somme des deux notes est %d\n",s);
     m=s/2;
     printf ("la moyenn est %d\n",m);
     return (0);
