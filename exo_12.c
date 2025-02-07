2770ae3e00633900a906abe0565b0e1f7f66ae2c
TsikyAndriatahiana <antsah.andriatahina@gmail.com>
2025-02-07 10:21:47 +0300

    Update exo_12.c

--- exo_12.c
+++ exo_12.c
@@ -4,6 +4,12 @@ int main ()
     int a,s,i;
     printf ("saisir un nombre");
     scanf ("%d",&a);
+    for (i=1;i<a;i++)
+    {
+      printf ("%d+",i);
+    }
+      printf ("%d=",a);
+
     s=0;
     for (i=1;i<=a;i++)
     {
