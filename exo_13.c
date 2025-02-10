5c6e48f36da19cb16859ba7432c1505519bcaddf
TsikyAndriatahiana <antsah.andriatahina@gmail.com>
2025-02-10 09:00:32 +0300

    Update exo_13.c

--- exo_13.c
+++ exo_13.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-    int a,i;
+    int a,f,i;
     printf ("saisir un nombre");
     scanf ("%d",&a);
     for (i=1;i<a;i++)
@@ -9,6 +9,11 @@ int main ()
         printf ("%d*",i);
     
     }
-    printf ("%d",a);
+    printf ("%d=",a);
+for (i=1;i<=a;i++)
+    {
+      f=f*i;
+    }
+       printf ("%d",f);
     return (0);
 }
